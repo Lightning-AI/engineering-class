10305a1d78fbe5974189c7c1481fe3e518a6a973
rasbt <mail@sebastianraschka.com>
2022-05-19 15:57:48 -0500

    episode 1 supplementary

---
+++ ep01-nbs-vs-projects/project/src/mypackage/mycode.py
@@ -0,0 +1,2 @@
+def hello_world_func():
+    return 'hello world'
---
+++ ep01-nbs-vs-projects/project/src/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+
+setuptools.setup(
+    name='mypackage',
+    version='0.1',
+    author='sebastian',
+    packages=setuptools.find_packages(),
+)
